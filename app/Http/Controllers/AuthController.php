b3b1a69588204d9fc711e5acd72ce99411725d48
Japa Alekhin Llemos <jap@alekhin.io>
2022-05-14 10:55:16 +0800

    Added authentication UI.
    Signed-off-by: Japa Alekhin Llemos <jap@alekhin.io>

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Auth\ConfirmPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
+use Illuminate\Support\Facades\Route;
+
+class AuthController extends Controller
+{
+    public static function setupRoutes()
+    {
+        Route::controller(LoginController::class)->group(function () {
+            Route::get('/sign-in', 'showLoginForm')->name('login');
+            Route::post('/sign-in', 'login')->name('postLogin');
+            Route::post('/sign-out', 'logout')->name('logout');
+        });
+
+        Route::controller(RegisterController::class)->group(function () {
+            Route::get('/sign-up', 'showRegistrationForm')->name('register');
+            Route::post('/sign-up', 'register');
+        });
+
+        Route::controller(ForgotPasswordController::class)->group(function () {
+            Route::get('/password/reset', 'showLinkRequestForm')->name('password.request');
+            Route::post('/password/email', 'sendResetLinkEmail')->name('password.email');
+        });
+        Route::controller(ResetPasswordController::class)->group(function () {
+            Route::get('/password/reset/{token}', 'showResetForm')->name('password.reset');
+            Route::post('/password/reset', 'reset')->name('password.update');
+        });
+
+        Route::controller(ConfirmPasswordController::class)->group(function () {
+            Route::get('/password/confirm', 'showConfirmForm')->name('password.confirm');
+            Route::post('/password/confirm', 'confirm');
+        });
+
+        Route::controller(VerificationController::class)->group(function () {
+            Route::get('/email/verify', 'show')->name('verification.notice');
+            Route::get('/email/verify/{id}/{hash}', 'verify')->name('verification.verify');
+            Route::post('/email/resend', 'resend')->name('verification.resend');
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 
 /*
@@ -14,3 +15,4 @@ use App\Http\Controllers\IndexController;
 */
 
 IndexController::setupRoutes();
+AuthController::setupRoutes();
