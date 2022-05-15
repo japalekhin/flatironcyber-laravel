bb392d4c3be9636efc312c8117dd08bdb663fcec
Japa Alekhin Llemos <jap@alekhin.io>
2022-05-15 14:57:17 +0800

    Fixed sign in and sign up views.
    Signed-off-by: Japa Alekhin Llemos <jap@alekhin.io>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,25 +2,27 @@
 
 @section('content')
     <div class="container flex-grow flex flex-col items-center justify-center">
-        <div class="w-full max-w-sm bg-primary/10 px-6 py-8 border border-gray-300 rounded shadow-lg">
-            <div class="text-xl">
+        <div class="auth-card">
+            <div class="text-xl mb-6">
                 <i class="fa-solid fa-shield-alt"></i>
                 {{ __('Sign in to your account') }}
             </div>
 
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('postLogin') }}">
                 @csrf
 
-                <div class="input-field">
-                    <input type="email" id="email" class="peer" placeholder="johndoe@example.com" />
+                <div class="input-field input-with-label">
+                    <input type="email" id="email" name="email" class="peer" value="{{ old('email') }}"
+                        placeholder="{{ __('Email') }}" required />
                     <label for="email">{{ __('Email') }}</label>
                     @error('email')
                         <span class="error-message" role="alert">{{ $message }}</span>
                     @enderror
                 </div>
 
-                <div class="input-field" password-toggle>
-                    <input type="password" id="password" class="peer" placeholder="secret" password-toggle-input />
+                <div class="input-field input-with-label" password-toggle>
+                    <input type="password" id="password" name="password" class="peer" placeholder="{{ __('Password') }}" required
+                        password-toggle-input />
                     <label for="password">{{ __('Password') }}</label>
                     <button type="button" tabindex="-1" password-toggle-button>
                         <i class="fa-solid fa-fw fa-eye-slash"></i>
@@ -30,16 +32,18 @@
                     @enderror
                     @if (Route::has('password.request'))
                         <div class="mt-1">
-                            <a class="btn btn-link"
-                                href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <i class="fa-solid fa-circle-info text-gray-400"></i>
+                                {{ __('Forgot Your Password?') }}
+                            </a>
                         </div>
                     @endif
                 </div>
 
                 <div class="check-field">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                    <input type="checkbox" id="remember" name="remember" class="form-check-input"
                         {{ old('remember') ? 'checked' : '' }}>
-                    <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
+                    <label for="remember">{{ __('Remember Me') }}</label>
                 </div>
 
                 <button type="submit" class="button button-primary block w-full m-field-row">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,74 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="container flex-grow flex flex-col items-center justify-center">
+        <div class="auth-card">
+            <div class="text-xl mb-6">
+                <i class="fa-solid fa-user-plus"></i>
+                {{ __('Create a new account') }}
             </div>
+
+            <form method="POST" action="{{ route('postRegister') }}">
+                @csrf
+
+                <div class="input-field input-with-label">
+                    <input type="text" id="first_name" name="first_name" class="peer"
+                        value="{{ old('first_name') }}" placeholder="{{ __('First Name') }}" required autocomplete="first_name" autofocus>
+                    <label for="first_name">{{ __('First Name') }}</label>
+                    @error('first_name')
+                        <span class="error-message" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="input-field input-with-label">
+                    <input type="text" id="last_name" name="last_name" class="peer"
+                        value="{{ old('last_name') }}" placeholder="{{ __('Last Name') }}" required autocomplete="last_name" autofocus>
+                    <label for="last_name">{{ __('Last Name') }}</label>
+                    @error('last_name')
+                        <span class="error-message" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="input-field input-with-label">
+                    <input type="email" id="email" name="email" class="peer" value="{{ old('email') }}" placeholder="{{ __('Email') }}" required />
+                    <label for="email">{{ __('Email') }}</label>
+                    @error('email')
+                        <span class="error-message" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="input-field input-with-label" password-toggle>
+                    <input type="password" id="password" name="password" class="peer" placeholder="{{ __('Password') }}" required password-toggle-input />
+                    <label for="password">{{ __('Password') }}</label>
+                    <button type="button" tabindex="-1" password-toggle-button>
+                        <i class="fa-solid fa-fw fa-eye-slash"></i>
+                    </button>
+                    @error('password')
+                        <span class="text-danger text-sm font-light" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="input-field input-with-label" password-toggle>
+                    <input type="password" id="password_confirmation" name="password_confirmation" class="peer" placeholder="{{ __('Confirm Password') }}" required password-toggle-input />
+                    <label for="password_confirmation">{{ __('Confirm Password') }}</label>
+                    <button type="button" tabindex="-1" password-toggle-button>
+                        <i class="fa-solid fa-fw fa-eye-slash"></i>
+                    </button>
+                    @error('password_confirmation')
+                        <span class="text-danger text-sm font-light" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <button type="submit" class="button button-primary block w-full m-field-row">
+                    <i class="fa-solid fa-check-circle"></i>
+                    {{ __('Sign Up') }}
+                </button>
+
+                <div class="m-field-row">
+                    {{ __('Already have an account?') }}
+                    <a href="{{ route('login') }}">{{ __('Sign In') }}</a>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
