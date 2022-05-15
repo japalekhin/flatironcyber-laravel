59f0ad3e0701235be36035df1dc7ff08336b4d59
Japa Alekhin Llemos <jap@alekhin.io>
2022-05-15 15:15:06 +0800

    Separated footer into its own partial.
    Signed-off-by: Japa Alekhin Llemos <jap@alekhin.io>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,9 +11,5 @@
 @endsection
 
 @section('footer')
-    <footer class="bg-gray-300">
-        <div class="container text-center mx-auto my-4">
-            Copyright <a href="{{ route('index') }}">FlatIron Cyber</a> {{ date('Y') }}. All rights reserved.
-        </div>
-    </footer>
+    @include('partials.site-footer')
 @endsection
---
+++ resources/views/partials/site-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-gray-300">
+    <div class="container text-center mx-auto my-4">
+        Copyright <a href="{{ route('index') }}">FlatIron Cyber</a> {{ date('Y') }}. All rights reserved.
+    </div>
+</footer>
