f2ae7dbdd8a8199cccd5d41d61ad4b1bbb835914
Japa Alekhin Llemos <jap@alekhin.io>
2022-05-15 13:39:29 +0800

    Fixed header navigation links and site title.
    Signed-off-by: Japa Alekhin Llemos <jap@alekhin.io>

--- resources/views/partials/header-navigation.blade.php
+++ resources/views/partials/header-navigation.blade.php
@@ -2,17 +2,17 @@
     <div class="flex flex-row items-center justify-between container my-4">
         <nav class="font-medium">
             <a href="{{ route('index') }}">
-                <span class="">FlatIron Cyber</span>
+                <span class="">{{ env('APP_NAME', __('FlatIron Cyber')) }}</span>
             </a>
         </nav>
         <nav class="flex flex-row gap-4 items-center font-normal">
-            <a href="/" class="">
+            <a href="{{ route('login') }}" class="">
                 <i class="fa-solid fa-sign-in-alt mr-2"></i>
-                <span>Sign In</span>
+                <span>{{ __('Sign In') }}</span>
             </a>
-            <a href="/" class="">
+            <a href="{{ route('register') }}" class="">
                 <i class="fa-solid fa-user-plus mr-2"></i>
-                <span>Sign Up</span>
+                <span>{{ __('Sign Up') }}</span>
             </a>
         </nav>
     </div>
